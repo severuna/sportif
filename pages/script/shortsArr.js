da5883a951bd7d46e52264224eaec732dfce4b3e
Severuna <oxxxyfomenkova@gmail.com>
2023-02-05 22:22:10 +0200

    filling the array with elements and their values

--- pages/script/shortsArr.js
+++ pages/script/shortsArr.js
@@ -1,11 +1,452 @@
 const shortsArr = [
     {
-        id: '',
-        src: "",
-        title: "",
-        size: [],
-        colors: [],
-        price: "",
-        rating: ""
+        id: 0,
+        src: "./images/shorts/short01.png",
+        title: "Short 01",
+        size: [32, 33, 24, 35, 36],
+        colors: ['cadetblue', 'tomato', 'black', 'pink', 'white', 'turquoise', 'crimson'],
+        price: "35.99", 
+        rating: 5
+    },
+    {
+        id: 1,
+        src: "./images/shorts/short02.png",
+        title: "Short 02",
+        size: [32, 33, 24, 35, 36],
+        colors: ['darkolivegreen', 'fuchsia', 'teal', 'thistle', 'tan', 'greenyellow', 'violet', 'gray', 'forestgreen', 'crimson', 'black'],
+        price: "32.99",
+        rating: 5
+    }, 
+    {
+        id: 2,
+        src: "./images/shorts/short03.png",
+        title: "Short 03",
+        size: [32, 33, 24, 35, 36],
+        colors: ['coral', 'skyblue', 'slateblue', 'brown', 'greenyellow', 'lightcoral', 'violet', 'seagreen', 'black', 'tan'], 
+        price: "29.99", 
+        rating: 4
+    }, 
+    {
+        id: 3,
+        src: "./images/shorts/short04.png",
+        title: "Short 04",
+        size: [32, 33, 24, 35, 36],
+        colors: ['black', 'slategrey', 'thistle', 'turquoise', 'chartreuse', 'crimson', 'forestgreen', 'gray', 'greenyellow', 'lightcoral'], 
+        price: "23.99", 
+        rating: 5
+    }, 
+    {
+        id: 4,
+        src: "./images/shorts/short05.png",
+        title: "Short 05",
+        size: [32, 33, 24, 35, 36],
+        colors: ['coral', 'sienna', 'lightcoral', 'gold', 'cadetblue', 'teal', 'seagreen'], 
+        price: "28.99", 
+        rating: 5
+    }, 
+    {
+        id: 5,
+        src: "./images/shorts/short06.png",
+        title: "Short 06",
+        size: [32, 33, 24, 35, 36],
+        colors: ['gray', 'black', 'red', 'purple', 'cadetblue'], 
+        price: "43.99", 
+        rating: 5
+    }, 
+    {
+        id: 6,
+        src: "./images/shorts/short07.png",
+        title: "Short 07",
+        size: [32, 33, 24, 35, 36],
+        colors: ['brown', 'seagreen', 'skyblue', 'slategrey', 'coral', 'crimson', 'gold', 'fuchsia', 'teal', 'tan'], 
+        price: "49.99", 
+        rating: 4
+    }, 
+    {
+        id: 7,
+        src: "./images/shorts/short08.png",
+        title: "Short 08",
+        size: [32, 33, 24, 35, 36],
+        colors: ['fuchsia', 'greenyellow', 'lightcoral', 'tan', 'yellowgreen', 'violet', 'thistle', 'teal', 'seagreen', 'cadetblue'], 
+        price: "36.99", 
+        rating:5
+    } ,
+    {
+        id: 8,
+        src: "./images/shorts/short09.png",
+        title: "Short 09",
+        size: [32, 33, 24, 35, 36],
+        colors: ['tan', 'seagreen', 'brown', 'chartreuse', 'darkolivegreen', 'greenyellow', 'lightcoral'], 
+        price: "35.00", 
+        rating: 5
+    }, 
+    {
+        id: 9,
+        src: "./images/shorts/short10.png",
+        title: "Short 10",
+        size: [32, 33, 24, 35, 36],
+        colors: ['gold', 'greenyellow', 'fuchsia', 'lightcoral', 'seagreen', 'sienna', 'slateblue', 'thistle', 'turquoise', 'yellowgreen'], 
+        price: "23.99", 
+        rating: 5
+    },
+    {
+        id: 10,
+        src: "./images/shorts/short11.png",
+        title: "Short 11",
+        size: [32, 33, 24, 35, 36],
+        colors: ['turquoise', 'brown', 'darkolivegreen', 'darkgoldenrod', 'cadetblue', 'fuchsia', 'lightcoral', 'gray', 'skyblue', 'thistle'], 
+        price: "33.30", 
+        rating: 4
+    }, 
+    {
+        id: 11,
+        src: "./images/shorts/short12.jpg",
+        title: "Short 12",
+        size: [32, 33, 24, 35, 36],
+        colors: ['slateblue', 'coral', 'crimson', 'forestgreen', 'gray', 'gold', 'slategrey', 'yellowgreen'], 
+        price: "29.99", 
+        rating: 4
+    }, 
+    {
+        id: 12,
+        src: "./images/shorts/short13.jpg",
+        title: "Short 13",
+        size: [32, 33, 24, 35, 36],
+        colors: ['slategrey', 'black', 'coral', 'skyblue', 'violet', 'darkolivegreen'], 
+        price: "28.99", 
+        rating: 5
+    },
+    {
+        id: 13,
+        src: "./images/shorts/short14.jpg",
+        title: "Short 14",
+        size: [32, 33, 24, 35, 36],
+        colors: ['seagreen', 'gold', 'lightcoral', 'gray', 'brown', 'teal'], 
+        price: "27.99", 
+        rating: 5
+    },
+    {
+        id: 14,
+        src: "./images/shorts/short15.jpg",
+        title: "Short 15",
+        size: [32, 33, 24, 35, 36],
+        colors: ['gray', 'teal', 'turquoise', 'tan', 'sienna'], 
+        price: "28.99", 
+        rating: 5 
+    },
+    {
+        id: 15,
+        src: "./images/shorts/short16.jpeg",
+        title: "Short 16",
+        size: [32, 33, 24, 35, 36],
+        colors: ['darkolivegreen', 'darkgoldenrod', 'gray', 'slateblue', 'slategrey', 'thistle', 'cadetblue'], 
+        price: "29.99", 
+        rating: 5
+    },
+    {
+        id: 16,
+        src: "./images/shorts/short17.jpg",
+        title: "Short 17",
+        size: [32, 33, 24, 35, 36],
+        colors: ['darkolivegreen', 'thistle', 'slateblue', 'coral', 'tan', 'turquoise'], 
+        price: "23.99", 
+        rating: 3
+    },
+    {
+        id: 17,
+        src: "./images/shorts/short18.jpg",
+        title: "Short 18",
+        size: [32, 33, 24, 35, 36],
+        colors: ['chartreuse', 'fuchsia', 'yellowgreen', 'violet', 'seagreen'], 
+        price: "22.99", 
+        rating: 2
+    },
+    {
+        id: 18,
+        src: "./images/shorts/short19.jpg",
+        title: "Short 19",
+        size: [32, 33, 24, 35, 36],
+        colors: ['tan', 'darkgoldenrod', 'forestgreen', 'gray', 'greenyellow', 'lightcoral', 'slateblue', 'slategrey', 'violet', 'yellowgreen'], 
+        price: "21.99", 
+        rating: 5
+    }, 
+    {
+        id: 19,
+        src: "./images/shorts/short20.jpg",
+        title: "Short 20",
+        size: [32, 33, 24, 35, 36],
+        colors: ['coral', 'crimson', 'fuchsia', 'violet', 'lightcoral', 'sienna'], 
+        price: "18.99", 
+        rating: 5
+    },
+    {
+        id: 20,
+        src: "./images/shorts/short21.jpg",
+        title: "Short 21",
+        size: [32, 33, 24, 35, 36],
+        colors: ['yellowgreen', 'seagreen', 'chartreuse', 'forestgreen', 'greenyellow', 'darkolivegreen'], 
+        price: "15.99", 
+        rating: 5
+    },
+    {
+        id: 21,
+        src: "./images/shorts/short22.jpeg",
+        title: "Short 22",
+        size: [32, 33, 24, 35, 36],
+        colors: ['lightcoral', 'greenyellow', 'cadetblue', 'skyblue', 'violet', 'yellowgreen'], 
+        price: "33.99", 
+        rating: 4
+    },
+    {
+        id: 22,
+        src: "./images/shorts/short23.jpg",
+        title: "Short 23",
+        size: [32, 33, 24, 35, 36],
+        colors: ['black', 'red', 'yellow', 'brown', 'coral', 'gray', 'white', 'azure'], 
+        price: "35.99", 
+        rating: 3
+    },
+    {
+        id: 23,
+        src: "./images/shorts/short24.jpg",
+        title: "Short 24",
+        size: [32, 33, 24, 35, 36],
+        colors: ['thistle', 'coral', 'cadetblue', 'seagreen', 'tan'], 
+        price: "49.99", 
+        rating: 5
+    },
+    {
+        id: 24,
+        src: "./images/shorts/short25.jpg",
+        title: "Short 25",
+        size: [32, 33, 24, 35, 36],
+        colors: ['seagreen', 'crimson'], 
+        price: "30.99", 
+        rating: 4
+    },
+    {
+        id: 25,
+        src: "./images/shorts/short26.png",
+        title: "Short 26",
+        size: [32, 33, 24, 35, 36],
+        colors: ['brown', 'crimson', 'fuchsia', 'red', 'violet', 'gold', 'forestgreen', 'turquoise', 'lightcoral', 'coral'], 
+        price: "21.99", 
+        rating: 4
+    },
+    {
+        id: 26,
+        src: "./images/shorts/short27.jpg",
+        title: "Short 26",
+        size: [32, 33, 24, 35, 36],
+        colors: ['brown', 'crimson', 'fuchsia', 'red', 'violet', 'gold', 'forestgreen', 'turquoise', 'lightcoral', 'coral'], 
+        price: "21.99", 
+        rating: 4
+    },
+    {
+        id: 27,
+        src: "./images/shorts/short28.jpg",
+        title: "Short 28",
+        size: [32, 33, 24, 35, 36],
+        colors: ['darkolivegreen', 'lightcoral'], 
+        price: "53.99", 
+        rating: 2
+    }, 
+    {
+        id: 28,
+        src: "./images/shorts/short29.jpg",
+        title: "Short 29",
+        size: [32, 33, 24, 35, 36],
+        colors: ['fuchsia', 'skyblue', 'slategrey', 'greenyellow'], 
+        price: "33.99", 
+        rating: 5
+    },
+    {
+        id: 29,
+        src: "./images/shorts/short30.jpg",
+        title: "Short 30",
+        size: [32, 33, 24, 35, 36],
+        colors: ['fuchsia', 'greenyellow', 'lightcoral', 'darkolivegreen'], 
+        price: "34.99", 
+        rating: 5
+    }, 
+    {
+        id: 30,
+        src: "./images/shorts/short31.jpg",
+        title: "Short 31",
+        size: [32, 33, 24, 35, 36],
+        colors: ['gold', 'chartreuse', 'seagreen', 'sienna', 'skyblue', 'slateblue', 'fuchsia'], 
+        price: "30.99", 
+        rating: 5
+    },
+    {
+        id: 31,
+        src: "./images/shorts/short32.jpeg",
+        title: "Short 32",
+        size: [32, 33, 24, 35, 36],
+        colors: ['seagreen', 'skyblue', 'gray', 'forestgreen', 'slateblue', 'lightcoral'], 
+        price: "29.99", 
+        rating: 5
+    },
+    {
+        id: 32,
+        src: "./images/shorts/short33.png",
+        title: "Short 33",
+        size: [32, 33, 24, 35, 36],
+        colors: ['darkgoldenrod', 'chartreuse', 'sienna'], 
+        price: "29.99", 
+        rating: 5
+    }, 
+    {
+        id: 33,
+        src: "./images/shorts/short34.jpg",
+        title: "Short 34",
+        size: [32, 33, 24, 35, 36],
+        colors: ['fuchsia', 'lightcoral', 'coral', 'slateblue', 'crimson'], 
+        price: "28.99", 
+        rating: 5
+    },
+    {
+        id: 34,
+        src: "./images/shorts/short35.jpg",
+        title: "Short 35",
+        size: [32, 33, 24, 35, 36],
+        colors: ['gold', 'yellowgreen', 'violet', 'turquoise', 'thistle', 'tan', 'teal', 'slategrey', 'slateblue', 'skyblue'], 
+        price: "27.99", 
+        rating: 5
+    },
+    {
+        id: 35,
+        src: "./images/shorts/short36.jpg",
+        title: "Short 36",
+        size: [32, 33, 24, 35, 36],
+        colors: ['sienna', 'seagreen', 'brown', 'cadetblue', 'thistle', 'darkgoldenrod'], 
+        price: "26.99", 
+        rating: 4
+    },
+    {
+        id: 36,
+        src: "./images/shorts/short37.jpg",
+        title: "Short 37",
+        size: [32, 33, 24, 35, 36],
+        colors: ['lightcoral', 'greenyellow', 'gray', 'gold', 'forestgreen', 'fuchsia', 'darkgoldenrod', 'darkolivegreen', 'crimson', 'coral'], 
+        price: "25.99", 
+        rating: 4
+    },
+    {
+        id: 37,
+        src: "./images/shorts/short38.jpg",
+        title: "Short 38",
+        size: [32, 33, 24, 35, 36],
+        colors: ['teal', 'slategrey', 'sienna', 'brown', 'chartreuse', 'coral', 'crimson', 'darkolivegreen', 'darkgoldenrod', 'forestgreen'], 
+        price: "25.99", 
+        rating: 3
+    },
+    {
+        id: 38,
+        src: "./images/shorts/short39.jpg",
+        title: "Short 39",
+        size: [32, 33, 24, 35, 36],
+        colors: ['coral', 'skyblue', 'tan', 'turquoise'], 
+        price: "23.99", 
+        rating: 5
+    },
+    {
+        id: 39,
+        src: "./images/shorts/short40.jpg",
+        title: "Short 40",
+        size: [32, 33, 24, 35, 36],
+        colors: ['yellowgreen', 'slategrey', 'slateblue', 'chartreuse', 'darkolivegreen'], 
+        price: "23.99", 
+        rating: 5
+    },
+    {
+        id: 40,
+        src: "./images/shorts/short41.jpg",
+        title: "Short 41",
+        size: [32, 33, 24, 35, 36],
+        colors: ['brown', 'crimson', 'red', 'fuchsia', 'violet'], 
+        price: "24.99", 
+        rating: 5
+    },
+    {
+        id: 41,
+        src: "./images/shorts/short42.jpg",
+        title: "Short 42",
+        size: [32, 33, 24, 35, 36],
+        colors: ['coral', 'sienna', 'brown', 'gray'], 
+        price: "25.99", 
+        rating: 4
+    },
+    {
+        id: 42,
+        src: "./images/shorts/short43.jpeg",
+        title: "Short 43",
+        size: [32, 33, 24, 35, 36],
+        colors: ['darkolivegreen', 'gold', 'greenyellow', 'seagreen', 'slategrey', 'teal'], 
+        price: "23.99", 
+        rating: 4
+    },
+    {
+        id: 43,
+        src: "./images/shorts/short44.jpg",
+        title: "Short 44",
+        size: [32, 33, 24, 35, 36],
+        colors: ['crimson', 'black', 'gray', 'tan', 'turquoise', 'violet'], 
+        price: "24.99", 
+        rating: 4
+    }, 
+    {
+        id: 44,
+        src: "./images/shorts/short45.jpg",
+        title: "Short 45",
+        size: [32, 33, 24, 35, 36],
+        colors: ['sienna', 'tan', 'turquoise', 'coral', 'darkolivegreen'], 
+        price: "25.99", 
+        rating: 4
+    },
+    {
+        id: 45,
+        src: "./images/shorts/short46.jpg",
+        title: "Short 46",
+        size: [32, 33, 24, 35, 36],
+        colors: ['slateblue', 'slategrey', 'teal', 'thistle'], 
+        price: "26.99", 
+        rating: 5
+    },
+    {
+        id: 46,
+        src: "./images/shorts/short47.jpg",
+        title: "Short 47",
+        size: [32, 33, 24, 35, 36],
+        colors: ['gray', 'gold', 'greenyellow', 'lightcoral', 'coral', 'slateblue', 'teal'], 
+        price: "27.99", 
+        rating: 5
+    },
+    {
+        id: 47,
+        src: "./images/shorts/short48.png",
+        title: "Short 48",
+        size: [32, 33, 24, 35, 36],
+        colors: ['coral', 'skyblue', 'tan', 'turquoise', 'yellowgreen', 'violet', 'brown', 'teal', 'crimson', 'darkolivegreen'], 
+        price: "27.99", 
+        rating: 5
+    },
+    {
+        id: 48,
+        src: "./images/shorts/short49.jpg",
+        title: "Short 49",
+        size: [32, 33, 24, 35, 36],
+        colors: ['slategrey', 'darkolivegreen', 'darkgoldenrod', 'fuchsia', 'turquoise', 'yellowgreen', 'violet', 'thistle', 'tan', 'teal'], 
+        price: "28.99", 
+        rating: 5
+    },
+    {
+        id: 49,
+        src: "./images/shorts/short50.jpg",
+        title: "Short 08",
+        size: [32, 33, 24, 35, 36],
+        colors: ['lightcoral', 'greenyellow', 'gray', 'gold', 'forestgreen', 'fuchsia', 'darkgoldenrod', 'darkolivegreen', 'crimson', 'coral'], 
+        price: "29.99", 
+        rating: 5
     }
 ]
