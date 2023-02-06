9c65c1464122bac682372db98ccbe1357d2a5927
Severuna <oxxxyfomenkova@gmail.com>
2023-02-06 21:14:45 +0200

    add shirts array data

---
+++ pages/script/shirtsArr.js
@@ -0,0 +1,452 @@
+export const shirtsArr = [
+    {
+        id: 0,
+        src: "./images/shirts/shirt01.jpg",
+        title: "Shirt 01",
+        size: [32, 33, 24, 35, 36],
+        colors: ['cadetblue', 'tomato', 'black', 'pink', 'white', 'turquoise', 'crimson'],
+        price: "35.99", 
+        rating: 5
+    },
+    {
+        id: 1,
+        src: "./images/shirts/shirt02.jpg",
+        title: "Shirt 02",
+        size: [32, 33, 24, 35, 36],
+        colors: ['darkolivegreen', 'fuchsia', 'teal', 'thistle', 'tan', 'greenyellow', 'violet', 'gray', 'forestgreen', 'crimson', 'black'],
+        price: "32.99",
+        rating: 5
+    }, 
+    {
+        id: 2,
+        src: "./images/shirts/shirt03.jpg",
+        title: "Shirt 03",
+        size: [32, 33, 24, 35, 36],
+        colors: ['coral', 'skyblue', 'slateblue', 'brown', 'greenyellow', 'lightcoral', 'violet', 'seagreen', 'black', 'tan'], 
+        price: "29.99", 
+        rating: 4
+    }, 
+    {
+        id: 3,
+        src: "./images/shirts/shirt04.jpg",
+        title: "Shirt 04",
+        size: [32, 33, 24, 35, 36],
+        colors: ['black', 'slategrey', 'thistle', 'turquoise', 'chartreuse', 'crimson', 'forestgreen', 'gray', 'greenyellow', 'lightcoral'], 
+        price: "23.99", 
+        rating: 5
+    }, 
+    {
+        id: 4,
+        src: "./images/shirts/shirt05.jpg",
+        title: "Shirt 05",
+        size: [32, 33, 24, 35, 36],
+        colors: ['coral', 'sienna', 'lightcoral', 'gold', 'cadetblue', 'teal', 'seagreen'], 
+        price: "28.99", 
+        rating: 5
+    }, 
+    {
+        id: 5,
+        src: "./images/shirts/shirt06.jpg",
+        title: "Shirt 06",
+        size: [32, 33, 24, 35, 36],
+        colors: ['gray', 'black', 'red', 'purple', 'cadetblue'], 
+        price: "43.99", 
+        rating: 5
+    }, 
+    {
+        id: 6,
+        src: "./images/shirts/shirt07.jpg",
+        title: "Shirt 07",
+        size: [32, 33, 24, 35, 36],
+        colors: ['brown', 'seagreen', 'skyblue', 'slategrey', 'coral', 'crimson', 'gold', 'fuchsia', 'teal', 'tan'], 
+        price: "49.99", 
+        rating: 4
+    }, 
+    {
+        id: 7,
+        src: "./images/shirts/shirt08.jpg",
+        title: "Shirt 08",
+        size: [32, 33, 24, 35, 36],
+        colors: ['fuchsia', 'greenyellow', 'lightcoral', 'tan', 'yellowgreen', 'violet', 'thistle', 'teal', 'seagreen', 'cadetblue'], 
+        price: "36.99", 
+        rating:5
+    } ,
+    {
+        id: 8,
+        src: "./images/shirts/shirt09.jpg",
+        title: "Shirt 09",
+        size: [32, 33, 24, 35, 36],
+        colors: ['tan', 'seagreen', 'brown', 'chartreuse', 'darkolivegreen', 'greenyellow', 'lightcoral'], 
+        price: "35.00", 
+        rating: 5
+    }, 
+    {
+        id: 9,
+        src: "./images/shirts/shirt10.jpg",
+        title: "Shirt 10",
+        size: [32, 33, 24, 35, 36],
+        colors: ['gold', 'greenyellow', 'fuchsia', 'lightcoral', 'seagreen', 'sienna', 'slateblue', 'thistle', 'turquoise', 'yellowgreen'], 
+        price: "23.99", 
+        rating: 5
+    },
+    {
+        id: 10,
+        src: "./images/shirts/shirt11.jpg",
+        title: "Shirt 11",
+        size: [32, 33, 24, 35, 36],
+        colors: ['turquoise', 'brown', 'darkolivegreen', 'darkgoldenrod', 'cadetblue', 'fuchsia', 'lightcoral', 'gray', 'skyblue', 'thistle'], 
+        price: "33.30", 
+        rating: 4
+    }, 
+    {
+        id: 11,
+        src: "./images/shirts/shirt12.jpg",
+        title: "Shirt 12",
+        size: [32, 33, 24, 35, 36],
+        colors: ['slateblue', 'coral', 'crimson', 'forestgreen', 'gray', 'gold', 'slategrey', 'yellowgreen'], 
+        price: "29.99", 
+        rating: 4
+    }, 
+    {
+        id: 12,
+        src: "./images/shirts/shirt13.jpg",
+        title: "Shirt 13",
+        size: [32, 33, 24, 35, 36],
+        colors: ['slategrey', 'black', 'coral', 'skyblue', 'violet', 'darkolivegreen'], 
+        price: "28.99", 
+        rating: 5
+    },
+    {
+        id: 13,
+        src: "./images/shirts/shirt14.jpg",
+        title: "Shirt 14",
+        size: [32, 33, 24, 35, 36],
+        colors: ['seagreen', 'gold', 'lightcoral', 'gray', 'brown', 'teal'], 
+        price: "27.99", 
+        rating: 5
+    },
+    {
+        id: 14,
+        src: "./images/shirts/shirt15.jpg",
+        title: "Shirt 15",
+        size: [32, 33, 24, 35, 36],
+        colors: ['gray', 'teal', 'turquoise', 'tan', 'sienna'], 
+        price: "28.99", 
+        rating: 5 
+    },
+    {
+        id: 15,
+        src: "./images/shirts/shirt16.jpg",
+        title: "Shirt 16",
+        size: [32, 33, 24, 35, 36],
+        colors: ['darkolivegreen', 'darkgoldenrod', 'gray', 'slateblue', 'slategrey', 'thistle', 'cadetblue'], 
+        price: "29.99", 
+        rating: 5
+    },
+    {
+        id: 16,
+        src: "./images/shirts/shirt17.jpg",
+        title: "Shirt 17",
+        size: [32, 33, 24, 35, 36],
+        colors: ['darkolivegreen', 'thistle', 'slateblue', 'coral', 'tan', 'turquoise'], 
+        price: "23.99", 
+        rating: 3
+    },
+    {
+        id: 17,
+        src: "./images/shirts/shirt18.jpg",
+        title: "Shirt 18",
+        size: [32, 33, 24, 35, 36],
+        colors: ['chartreuse', 'fuchsia', 'yellowgreen', 'violet', 'seagreen'], 
+        price: "22.99", 
+        rating: 2
+    },
+    {
+        id: 18,
+        src: "./images/shirts/shirt19.jpg",
+        title: "Shirt 19",
+        size: [32, 33, 24, 35, 36],
+        colors: ['tan', 'darkgoldenrod', 'forestgreen', 'gray', 'greenyellow', 'lightcoral', 'slateblue', 'slategrey', 'violet', 'yellowgreen'], 
+        price: "21.99", 
+        rating: 5
+    }, 
+    {
+        id: 19,
+        src: "./images/shirts/shirt20.jpg",
+        title: "Shirt 20",
+        size: [32, 33, 24, 35, 36],
+        colors: ['coral', 'crimson', 'fuchsia', 'violet', 'lightcoral', 'sienna'], 
+        price: "18.99", 
+        rating: 5
+    },
+    {
+        id: 20,
+        src: "./images/shirts/shirt21.jpg",
+        title: "Shirt 21",
+        size: [32, 33, 24, 35, 36],
+        colors: ['yellowgreen', 'seagreen', 'chartreuse', 'forestgreen', 'greenyellow', 'darkolivegreen'], 
+        price: "15.99", 
+        rating: 5
+    },
+    {
+        id: 21,
+        src: "./images/shirts/shirt22.jpg",
+        title: "Shirt 22",
+        size: [32, 33, 24, 35, 36],
+        colors: ['lightcoral', 'greenyellow', 'cadetblue', 'skyblue', 'violet', 'yellowgreen'], 
+        price: "33.99", 
+        rating: 4
+    },
+    {
+        id: 22,
+        src: "./images/shirts/shirt23.jpg",
+        title: "Shirt 23",
+        size: [32, 33, 24, 35, 36],
+        colors: ['black', 'red', 'yellow', 'brown', 'coral', 'gray', 'white', 'azure'], 
+        price: "35.99", 
+        rating: 3
+    },
+    {
+        id: 23,
+        src: "./images/shirts/shirt24.jpg",
+        title: "Shirt 24",
+        size: [32, 33, 24, 35, 36],
+        colors: ['thistle', 'coral', 'cadetblue', 'seagreen', 'tan'], 
+        price: "49.99", 
+        rating: 5
+    },
+    {
+        id: 24,
+        src: "./images/shirts/shirt25.jpg",
+        title: "Shirt 25",
+        size: [32, 33, 24, 35, 36],
+        colors: ['seagreen', 'crimson'], 
+        price: "30.99", 
+        rating: 4
+    },
+    {
+        id: 25,
+        src: "./images/shirts/shirt26.jpg",
+        title: "Shirt 26",
+        size: [32, 33, 24, 35, 36],
+        colors: ['brown', 'crimson', 'fuchsia', 'red', 'violet', 'gold', 'forestgreen', 'turquoise', 'lightcoral', 'coral'], 
+        price: "21.99", 
+        rating: 4
+    },
+    {
+        id: 26,
+        src: "./images/shirts/shirt27.jpg",
+        title: "Shirt 26",
+        size: [32, 33, 24, 35, 36],
+        colors: ['brown', 'crimson', 'fuchsia', 'red', 'violet', 'gold', 'forestgreen', 'turquoise', 'lightcoral', 'coral'], 
+        price: "21.99", 
+        rating: 4
+    },
+    {
+        id: 27,
+        src: "./images/shirts/shirt28.jpg",
+        title: "Shirt 28",
+        size: [32, 33, 24, 35, 36],
+        colors: ['darkolivegreen', 'lightcoral'], 
+        price: "53.99", 
+        rating: 2
+    }, 
+    {
+        id: 28,
+        src: "./images/shirts/shirt29.jpg",
+        title: "Shirt 29",
+        size: [32, 33, 24, 35, 36],
+        colors: ['fuchsia', 'skyblue', 'slategrey', 'greenyellow'], 
+        price: "33.99", 
+        rating: 5
+    },
+    {
+        id: 29,
+        src: "./images/shirts/shirt30.jpg",
+        title: "Shirt 30",
+        size: [32, 33, 24, 35, 36],
+        colors: ['fuchsia', 'greenyellow', 'lightcoral', 'darkolivegreen'], 
+        price: "34.99", 
+        rating: 5
+    }, 
+    {
+        id: 30,
+        src: "./images/shirts/shirt31.jpg",
+        title: "Shirt 31",
+        size: [32, 33, 24, 35, 36],
+        colors: ['gold', 'chartreuse', 'seagreen', 'sienna', 'skyblue', 'slateblue', 'fuchsia'], 
+        price: "30.99", 
+        rating: 5
+    },
+    {
+        id: 31,
+        src: "./images/shirts/shirt32.png",
+        title: "Shirt 32",
+        size: [32, 33, 24, 35, 36],
+        colors: ['seagreen', 'skyblue', 'gray', 'forestgreen', 'slateblue', 'lightcoral'], 
+        price: "29.99", 
+        rating: 5
+    },
+    {
+        id: 32,
+        src: "./images/shirts/shirt33.jpg",
+        title: "Shirt 33",
+        size: [32, 33, 24, 35, 36],
+        colors: ['darkgoldenrod', 'chartreuse', 'sienna'], 
+        price: "29.99", 
+        rating: 5
+    }, 
+    {
+        id: 33,
+        src: "./images/shirts/shirt34.jpg",
+        title: "Shirt 34",
+        size: [32, 33, 24, 35, 36],
+        colors: ['fuchsia', 'lightcoral', 'coral', 'slateblue', 'crimson'], 
+        price: "28.99", 
+        rating: 5
+    },
+    {
+        id: 34,
+        src: "./images/shirts/shirt35.jpg",
+        title: "Shirt 35",
+        size: [32, 33, 24, 35, 36],
+        colors: ['gold', 'yellowgreen', 'violet', 'turquoise', 'thistle', 'tan', 'teal', 'slategrey', 'slateblue', 'skyblue'], 
+        price: "27.99", 
+        rating: 5
+    },
+    {
+        id: 35,
+        src: "./images/shirts/shirt36.jpg",
+        title: "Shirt 36",
+        size: [32, 33, 24, 35, 36],
+        colors: ['sienna', 'seagreen', 'brown', 'cadetblue', 'thistle', 'darkgoldenrod'], 
+        price: "26.99", 
+        rating: 4
+    },
+    {
+        id: 36,
+        src: "./images/shirts/shirt37.jpg",
+        title: "Shirt 37",
+        size: [32, 33, 24, 35, 36],
+        colors: ['lightcoral', 'greenyellow', 'gray', 'gold', 'forestgreen', 'fuchsia', 'darkgoldenrod', 'darkolivegreen', 'crimson', 'coral'], 
+        price: "25.99", 
+        rating: 4
+    },
+    {
+        id: 37,
+        src: "./images/shirts/shirt38.jpg",
+        title: "Shirt 38",
+        size: [32, 33, 24, 35, 36],
+        colors: ['teal', 'slategrey', 'sienna', 'brown', 'chartreuse', 'coral', 'crimson', 'darkolivegreen', 'darkgoldenrod', 'forestgreen'], 
+        price: "25.99", 
+        rating: 3
+    },
+    {
+        id: 38,
+        src: "./images/shirts/shirt39.jpg",
+        title: "Shirt 39",
+        size: [32, 33, 24, 35, 36],
+        colors: ['coral', 'skyblue', 'tan', 'turquoise'], 
+        price: "23.99", 
+        rating: 5
+    },
+    {
+        id: 39,
+        src: "./images/shirts/shirt40.jpg",
+        title: "Shirt 40",
+        size: [32, 33, 24, 35, 36],
+        colors: ['yellowgreen', 'slategrey', 'slateblue', 'chartreuse', 'darkolivegreen'], 
+        price: "23.99", 
+        rating: 5
+    },
+    {
+        id: 40,
+        src: "./images/shirts/shirt41.jpg",
+        title: "Shirt 41",
+        size: [32, 33, 24, 35, 36],
+        colors: ['brown', 'crimson', 'red', 'fuchsia', 'violet'], 
+        price: "24.99", 
+        rating: 5
+    },
+    {
+        id: 41,
+        src: "./images/shirts/shirt42.jpg",
+        title: "Shirt 42",
+        size: [32, 33, 24, 35, 36],
+        colors: ['coral', 'sienna', 'brown', 'gray'], 
+        price: "25.99", 
+        rating: 4
+    },
+    {
+        id: 42,
+        src: "./images/shirts/shirt43.jpg",
+        title: "Shirt 43",
+        size: [32, 33, 24, 35, 36],
+        colors: ['darkolivegreen', 'gold', 'greenyellow', 'seagreen', 'slategrey', 'teal'], 
+        price: "23.99", 
+        rating: 4
+    },
+    {
+        id: 43,
+        src: "./images/shirts/shirt44.jpg",
+        title: "Shirt 44",
+        size: [32, 33, 24, 35, 36],
+        colors: ['crimson', 'black', 'gray', 'tan', 'turquoise', 'violet'], 
+        price: "24.99", 
+        rating: 4
+    }, 
+    {
+        id: 44,
+        src: "./images/shirts/shirt45.jpg",
+        title: "Shirt 45",
+        size: [32, 33, 24, 35, 36],
+        colors: ['sienna', 'tan', 'turquoise', 'coral', 'darkolivegreen'], 
+        price: "25.99", 
+        rating: 4
+    },
+    {
+        id: 45,
+        src: "./images/shirts/shirt46.jpg",
+        title: "Shirt 46",
+        size: [32, 33, 24, 35, 36],
+        colors: ['slateblue', 'slategrey', 'teal', 'thistle'], 
+        price: "26.99", 
+        rating: 5
+    },
+    {
+        id: 46,
+        src: "./images/shirts/shirt47.jpg",
+        title: "Shirt 47",
+        size: [32, 33, 24, 35, 36],
+        colors: ['gray', 'gold', 'greenyellow', 'lightcoral', 'coral', 'slateblue', 'teal'], 
+        price: "27.99", 
+        rating: 5
+    },
+    {
+        id: 47,
+        src: "./images/shirts/shirt48.jpg",
+        title: "Shirt 48",
+        size: [32, 33, 24, 35, 36],
+        colors: ['coral', 'skyblue', 'tan', 'turquoise', 'yellowgreen', 'violet', 'brown', 'teal', 'crimson', 'darkolivegreen'], 
+        price: "27.99", 
+        rating: 5
+    },
+    {
+        id: 48,
+        src: "./images/shirts/shirt49.jpg",
+        title: "Shirt 49",
+        size: [32, 33, 24, 35, 36],
+        colors: ['slategrey', 'darkolivegreen', 'darkgoldenrod', 'fuchsia', 'turquoise', 'yellowgreen', 'violet', 'thistle', 'tan', 'teal'], 
+        price: "28.99", 
+        rating: 5
+    },
+    {
+        id: 49,
+        src: "./images/shirts/shirt50.jpg",
+        title: "Shirt 50",
+        size: [32, 33, 24, 35, 36],
+        colors: ['lightcoral', 'greenyellow', 'gray', 'gold', 'forestgreen', 'fuchsia', 'darkgoldenrod', 'darkolivegreen', 'crimson', 'coral'], 
+        price: "29.99", 
+        rating: 5
+    }
+]
