ef569bc85eb7d697df4f34bfd86ce922aa445bdb
Severuna <oxxxyfomenkova@gmail.com>
2023-02-09 22:05:00 +0200

    add a toggle method for the button - styling

--- pages/script/showStyle.js
+++ pages/script/showStyle.js
@@ -1,6 +1,7 @@
 export const hideContainer = (element, arrow) => {
     arrow.addEventListener('click', e => {
         e.preventDefault();
+        arrow.classList.toggle('revert');
         element.classList.toggle('none');
     })
 }
