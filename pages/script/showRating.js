b95b4d2af26c40bb4d50067dc1c250a24142db45
Severuna <oxxxyfomenkova@gmail.com>
2023-02-12 13:54:26 +0200

    correct variable name

--- pages/script/showRating.js
+++ pages/script/showRating.js
@@ -1,17 +1,17 @@
 export const showRating = (rating) => {
-    const listRating = document.createElement('div');
-    listRating.classList = 'list-item__rating flex-row';
+    const itemRating = document.createElement('div');
+    itemRating.classList = 'list-item__rating flex-row';
     const gold = Number(rating);
     const gray = 5 - Number(rating);
     for (let i = 0; i < gold; i++) {
         const goldRating = document.createElement('img');
         goldRating.src = './images/star_gold.svg';
-        listRating.append(goldRating)
+        itemRating.append(goldRating)
     }
     for (let i = 0; i < gray; i++) {
         const grayRating = document.createElement('img');
         grayRating.src = './images/star_gray.svg';
-        listRating.append(grayRating)
+        itemRating.append(grayRating)
     }
-    return listRating
+    return itemRating
 }
