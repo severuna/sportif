39e6744280803c7fdaccf6b8f71e84f9b8095417
Severuna <oxxxyfomenkova@gmail.com>
2023-02-12 17:55:39 +0200

    add a tooltip element and upload the element to local storage on click

--- pages/script/showTitle.js
+++ pages/script/showTitle.js
@@ -1,6 +1,13 @@
-export const showTitle = (title) => {
+export const showTitle = (element, title) => {
     const itemTitle = document.createElement('h3');
     itemTitle.classList = 'list-item__title';
     itemTitle.textContent = title;
+    const span = document.createElement('span')
+    span.textContent = "click for detailed information";
+    itemTitle.addEventListener('click', ()=> {
+        localStorage.setItem('element', JSON.stringify(element))
+        location.href = './product-card.html?'
+    })
+    itemTitle.append(span)
     return itemTitle
 }
