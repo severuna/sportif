52c8167ddab633d78985c2933b5eadb2c7e022af
Severuna <oxxxyfomenkova@gmail.com>
2023-02-12 17:35:57 +0200

    passing to the display function the title of all object fields

--- pages/script/showItem.js
+++ pages/script/showItem.js
@@ -8,6 +8,6 @@ import { showColors } from './showColors.js';
 export const showItem = (element) => {
     const listItem = document.createElement('div');
     listItem.classList = 'list-item flex-column';
-    listItem.append(showImg(element.src), showTitle(element.title), showRating(element.rating), showPrice(element.price), showColors(element.colors), showAddBag());
+    listItem.append(showImg(element.src), showTitle(element, element.title), showRating(element.rating), showPrice(element.price), showColors(element.colors), showAddBag());
     return listItem
 }
